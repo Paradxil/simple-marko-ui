47f3565a2bc16b98f1d98171fdf5e14d1010f6f0
Hunter Stratton <hstratton16@gmail.com>
2021-09-25 01:07:30 -0600

    Update to use date-fns.

--- components/ui-time.component.js
+++ components/ui-time.component.js
@@ -1,49 +1,17 @@
 const Base = require("./base.mixin");
+const formatDuration = require('date-fns/formatDuration/index.js');
+var millisecondsToHours = require('date-fns/millisecondsToHours/index.js')
+
 
 class Component {
     onCreate(input) {
         this.state = {
-            time: this.getNiceTime(input.milliseconds, input.showseconds||false)
+            time: formatDuration({hours: millisecondsToHours(input.milliseconds)})
         }
     }
 
     onInput(input) {
-        this.state.time = this.getNiceTime(input.milliseconds, input.showseconds||false);
-    }
-
-    getNiceTime(millseconds, full=false) {
-        let seconds = Math.floor(millseconds/1000);
-        let minutes = Math.floor(seconds/60);
-        let hours = Math.floor(minutes/60);
-        let days = Math.floor(hours/24);
-    
-        seconds = seconds - minutes*60;
-        minutes = minutes - hours*60;
-        hours = hours - days*24;
-    
-        let time = "";
-    
-        if(days > 0) {
-            time += days + " ";
-        }
-    
-        if(hours > 0 || full) {
-            time += hours + ":";
-        }
-    
-        if(hours > 0 || minutes > 0 || full) {
-            if(minutes < 10 && (hours > 0 || full)) {
-                time += "0" + minutes;
-            }
-            else {
-                time += minutes
-            }
-            time += ":";
-        }
-    
-        time += (seconds<10&&(minutes>0||hours>0||full))?"0"+seconds:seconds;
-    
-        return time;
+        this.state.time = formatDuration({hours: millisecondsToHours(input.milliseconds)});
     }
 };
 
